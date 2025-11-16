f214a5dc0a395a4cdb2a56967b7f72a296247b1d
bilalcoder-28 <bilalasif2007@gamil.com>
2025-11-17 00:24:49 +0500

    heloo

---
+++ app.js
@@ -0,0 +1,14 @@
+function generateRamdomNumber (){
+let ramdomPassowrd = "kwoiddwkdiwbaiwoubd"
+let password = ""
+for (let i = 0 ;i < 12 ; i++){
+    let ramdomNumber = Math.floor(Math.random() * ramdomPassowrd.length )
+    password = password + ramdomPassowrd[ramdomNumber]
+
+}
+console.log("password -->", password)
+let passwordBox =document.getElementById("password")
+passwordBox.innerText = `"The password is ${password} and the length is ${password.length}`;
+
+}
+
